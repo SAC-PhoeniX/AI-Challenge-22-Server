4e996743102b81988be303854525e936fa17ab4e
oralekin <oralekin@gmail.com>
2022-06-06 16:22:40 +0300

    change formatting on inference response

--- routes/infer.py
+++ routes/infer.py
@@ -23,6 +23,13 @@ def get_routes(race: Race) -> web.RouteTableDef:
         except KeyError:
             raise  web.HTTPBadRequest()
 
+    def make_infer_response(car_id, infer_result):
+        return {
+            "id": car_id,
+            "speed": infer_result[0],
+            "steering": infer_result[1]
+        }
+
 
     @routes.post("/infer/car/{car_id}")
     async def infer(request: Request):
@@ -30,16 +37,15 @@ def get_routes(race: Race) -> web.RouteTableDef:
         try:
             car = race.get_car_with_id(i["car_id"])
             inp = (await request.json())["rays"]
-            return json_response(car.infer(inp), dumps=dump_utf8)
+            return json_response(make_infer_response(car.id.hex, car.infer(inp)), dumps=dump_utf8)
         except KeyError:
             raise  web.HTTPBadRequest()
-    
 
     @routes.post("/infer/cars")
     async def infer(request: Request):
-        return json_response({
-            car_id: race.get_car_with_id(car_id).infer(inp) for car_id, inp in (await request.json()).items()
-        }, dumps=dump_utf8)
+        return json_response([
+            make_infer_response(car_id, race.get_car_with_id(car_id).infer(inp)) for car_id, inp in (await request.json()).items()
+        ], dumps=dump_utf8)
 
     return routes
 
